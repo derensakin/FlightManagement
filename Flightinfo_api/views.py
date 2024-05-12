7e420852d83362cc38275073c6eaf40aa12abbdb
cgb47 <161061122+cgb47@users.noreply.github.com>
2024-05-12 03:55:52 +0300

    Update views.py

--- Flightinfo_api/views.py
+++ Flightinfo_api/views.py
@@ -4,6 +4,32 @@ import json
 
 views = Blueprint('views', __name__)
 
+@views.route('/getFlight', methods=['POST'])
+def getFlight():
+    try:
+        flightID = request.form.get('flightID')  
+        conn = create_connection("localhost", "***", "***", "***", "1234")
+        flightDetails = findFlightByID(conn, flightID)
+        conn.close()
+        if flightDetails:
+            return jsonify({
+                "id": flightDetails[0],
+                "departure_date": flightDetails[1].strftime("%Y-%m-%d %H:%M:%S"),
+                "duration": flightDetails[2],
+                "distance": flightDetails[3],
+                "source_country": flightDetails[4],
+                "source_city": flightDetails[5],
+                "source_airport": flightDetails[6],
+                "source_airport_code": flightDetails[7],
+                "vehicle": flightDetails[8],
+                "shared_flight": flightDetails[9]
+            })
+        else:
+            return jsonify({"error": "Flight not found"}), 404
+    except Exception as e:
+        print(e)
+        return str(e), 500
+
 @views.route('/searchFlights', methods=['POST'])
 def searchFlights():
     try:
