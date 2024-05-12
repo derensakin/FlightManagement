b0b4ac5726351a996f3a26a24ab231ba8ab810de
cgb47 <161061122+cgb47@users.noreply.github.com>
2024-05-12 03:47:01 +0300

    Update views.py

--- Flightinfo_api/views.py
+++ Flightinfo_api/views.py
@@ -4,53 +4,44 @@ import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/getFlight', methods=['POST'])
-def getFlight():
+@views.route('/searchFlights', methods=['POST'])
+def searchFlights():
     try:
-        flightID = request.form.get('flightID')  
-        conn = create_connection("localhost", "***", "***", "***", "1234")
-        flightDetails = findFlightByID(conn, flightID)
-        conn.close()
-        if flightDetails:
-            return jsonify({
-                "id": flightDetails[0],
-                "departure_date": flightDetails[1].strftime("%Y-%m-%d %H:%M:%S"),
-                "duration": flightDetails[2],
-                "distance": flightDetails[3],
-                "source_country": flightDetails[4],
-                "source_city": flightDetails[5],
-                "source_airport": flightDetails[6],
-                "source_airport_code": flightDetails[7],
-                "vehicle": flightDetails[8],
-                "shared_flight": flightDetails[9]
-            })
-        else:
-            return jsonify({"error": "Flight not found"}), 404
-    except Exception as e:
-        print(e)
-        return str(e), 500
+        data = request.get_json()  # POST JSON verisini alır
+        filters = {
+            'id': data.get('id'),
+            'departure_date': data.get('departure_date'),
+            'duration': data.get('duration'),
+            'distance': data.get('distance'),
+            'source_country': data.get('source_country'),
+            'source_city': data.get('source_city'),
+            'source_airport': data.get('source_airport'),
+            'source_airport_code': data.get('source_airport_code'),
+            'vehicle': data.get('vehicle'),
+            'shared_flight': data.get('shared_flight')
+        }
 
-@views.route('/getAllFlights', methods=['GET'])
-def getAllFlights():
-    try:
+        # None değerleri filtrelerden temizleyin
+        filters = {k: v for k, v in filters.items() if v is not None}
+
         conn = create_connection("localhost", "***", "***", "***", "***")
-        flights = findAllFlights(conn)
+        flights = findFlightsAdvanced(conn, filters)
         conn.close()
-        flight_list = []
-        for flight in flights:
-            flight_list.append({
-                "id": flight[0],
-                "departure_date": flight[1].strftime("%Y-%m-%d %H:%M:%S"),
-                "duration": flight[2],
-                "distance": flight[3],
-                "source_country": flight[4],
-                "source_city": flight[5],
-                "source_airport": flight[6],
-                "source_airport_code": flight[7],
-                "vehicle": flight[8],
-                "shared_flight": flight[9]
-            })
-        return jsonify(flight_list)
+
+        results = [{
+            "id": flight[0],
+            "departure_date": flight[1].strftime("%Y-%m-%d %H:%M:%S"),
+            "duration": flight[2],
+            "distance": flight[3],
+            "source_country": flight[4],
+            "source_city": flight[5],
+            "source_airport": flight[6],
+            "source_airport_code": flight[7],
+            "vehicle": flight[8],
+            "shared_flight": flight[9]
+        } for flight in flights]
+
+        return jsonify(results)
     except Exception as e:
         print(e)
         return str(e), 500
