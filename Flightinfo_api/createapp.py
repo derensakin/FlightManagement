a1ef486529d271feffc29e5d67a465f561809e2f
cgb47 <161061122+cgb47@users.noreply.github.com>
2024-05-20 20:26:32 +0300

    Update createapp.py

--- Flightinfo_api/createapp.py
+++ Flightinfo_api/createapp.py
@@ -1,13 +1,11 @@
-import flask.app
 from flask import Flask
 from views import views
 
-
-def create_app()-> flask.app.Flask:
+def create_app() -> Flask:
     try:
         app = Flask(__name__)
         app.config['SECRET_KEY'] = 'SACRED_KEY'
-        app.register_blueprint(views,url_prefix='/')
+        app.register_blueprint(views, url_prefix='/')
         return app
     except Exception as e:
         print(e)
