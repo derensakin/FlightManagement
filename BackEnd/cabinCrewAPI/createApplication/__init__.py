3b50c554d368822506ec0d7bd7b8a2bebe85c51a
edirioz <143816500+egedirioz@users.noreply.github.com>
2024-05-26 21:10:52 +0300

    import CORS

--- BackEnd/cabinCrewAPI/createApplication/__init__.py
+++ BackEnd/cabinCrewAPI/createApplication/__init__.py
@@ -1,7 +1,11 @@
 import flask.app
 from flask import Flask
 from views import views
+from views import views
+from flask cors import CORS
 
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000","http://localhost:3001"])
 
 def create_app()-> flask.app.Flask:
     try:
