b0d584c3643174033416e3b2f65a9cb641e1c7c8
ismailAydinKarayilan <155107529+ismailAydinKarayilan@users.noreply.github.com>
2024-05-26 21:20:07 +0300

    Update createapp.py
    CORS eklendi

--- BackEnd/Flightinfo_api/createapp.py
+++ BackEnd/Flightinfo_api/createapp.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from views import views
+from flask_cors import CORS
 
 def create_app() -> Flask:
     try:
         app = Flask(__name__)
+        CORS(app, origihs=["http://localhost:3001"])
         app.config['SECRET_KEY'] = 'SACRED_KEY'
         app.register_blueprint(views, url_prefix='/')
         return app
